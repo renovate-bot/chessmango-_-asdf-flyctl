e87344f9b9e574dc8372859dfa098aadd8df81d8
Dimitris Karittevlis <safinn@users.noreply.github.com>
2024-08-15 11:03:46 +0100

    Filter tags to releases with assets (#8)
    * filter tags down to releases
    
    * stop up to latest version from api
    
    * fix linting with shfmt

--- lib/utils.bash
+++ lib/utils.bash
@@ -19,14 +19,27 @@ if [ -n "${GITHUB_API_TOKEN:-}" ]; then
 fi
 
 sort_versions() {
+  latest_version=$(
+    curl "https://api.fly.io/app/flyctl_releases/$(get_platform)/$(get_arch)/latest" |
+      grep -o 'v[0-9]\+\.[0-9]\+\.[0-9]\+' |
+      sed 's/^v//'
+  )
+
   sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
-    LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
+    LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n |
+    awk -v latest="$latest_version" '{
+      print $2
+      if ($2 == latest) {
+        exit
+      }
+    }'
 }
 
 list_github_tags() {
   git ls-remote --tags --refs "$GH_REPO" |
     grep -o 'refs/tags/.*' | cut -d/ -f3- |
-    sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
+    sed 's/^v//' |                  # NOTE: You might want to adapt this sed to remove non-version strings from tags
+    grep -Ev '^[0-9]{4}\.|[a-zA-Z]' # Removes tags starting with 4 digits followed by a dot and tags containing letters
 }
 
 list_all_versions() {
